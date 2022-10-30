fc823c0250f6d3751e4f343e0a97fd60a893669c
Vivek676 <77230002+Vivek676@users.noreply.github.com>
2022-10-30 11:41:13 +0530

    Add files via upload

---
+++ css/app.py
@@ -0,0 +1,16 @@
+num1 = int(input('Enter first no: '))
+num2 = int(input('Enter second no: '))
+op = input('Enter Operator: ')
+
+if op=='+':
+    num3=(num1+num2)
+    print(num3)
+elif op=='-':
+    num3=(num1-num2)
+    print(num3)
+elif op=='*':
+    num3=(num1*num2)
+    print(num3)
+elif op=='/':
+    num3=(num1/num2)
+    print(num3)
